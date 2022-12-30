0606d0bd6913e78eb0ea0f6e6c0ce743150d8e03
devCarlos <112131250+DevCarloss@users.noreply.github.com>
2022-12-30 15:08:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
+// Custom Server To Generate Ticket Acess Ubisoft Services 
 
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const ubiprod = require('./prod.json')
+const fetch = require('node-fetch')
+
+let v3sessions = {
+  "ubi": {
+    "sessions": "/v3/profiles/sessions"
+  }
+}
+
+
+// Get Ubisoft Ticket
+
+// Tickets Platform
+
+var platformtype = {
+  "All-Xbox": "xbl",
+  "All-Playstation": "psn",
+  "PC": "Uplay",
+  "Wiiu": "WiiU",
+  "NintendoSwitch": "Switch"
+  
+}
+app.post(v3sessions.ubi.sessions,(req,res) => {
+
+  var UbiServicesReq = fetch(ubiprod.prod.urls.UbiPROD + v3sessions.ubi.sessions,{
+    headers: {
+      "authorization": req.header('authorization'),
+    "Ubi-AppId": req.header('Ubi-AppId'),
+    "ubi-requestedplatformtype": req.header('ubi-requestedplatformtype'),
+    "Content-Type": req.header('Content-Type')
+    },
+    "method": "POST"
+  })
+  UbiServicesReq.then(response => response.json()).then(ticket => {
+    return res.send(ticket)
+    return res.status()})                   
+})
+
+// Listen
+
+app.listen(process.env.PORT ,console.log('Server Is Runnig'))
